630feaabe1faa5f0ac3a5989b51673fc02cf48c2
Ayush Rai <117967342+Tessstarkbankai@users.noreply.github.com>
2023-03-12 12:23:15 +0530

    1

---
+++ index.js
@@ -0,0 +1,71 @@
+var noOf = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOf; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+
+
+    this.style.color = "red";
+
+    var inerhtml = this.innerHTML;
+    makesound(inerhtml);
+    buttonAnimation(inerhtml);
+  })
+}
+
+document.addEventListener("keydown", function (event) {
+  makesound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makesound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      alert("tom 2 has been played");
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    case "l":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    default:
+      console.log("something is not right");
+      break;
+  }
+}
+function buttonAnimation(currentkey) {
+
+  document.querySelector("." + currentkey).classList.add("pressed");
+
+  setTimeout(function () {
+    document.querySelector("." + currentkey).classList.remove("pressed");
+
+  }, 200);
+}
